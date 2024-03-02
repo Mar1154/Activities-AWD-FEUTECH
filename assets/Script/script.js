2d4a2245fec81b7b610a8ebb54225d2ae61927c9
Mar <153974951+Mar1154@users.noreply.github.com>
2024-03-02 14:01:05 +0800

    Add files via upload

---
+++ assets/Script/script.js
@@ -0,0 +1,18 @@
+document.getElementById('_button2a').addEventListener('click', function(){
+    window.location.href = 'HoverVanish/hoverVanish.html';
+});
+document.getElementById('_button2b').addEventListener('click', function(){
+    window.location.href = 'JumpLetters/jumpLetters.html';
+});
+document.getElementById('_button2c').addEventListener('click', function(){
+    window.location.href = 'NotifBell/notifBell.html';
+});
+document.getElementById('_button2d').addEventListener('click', function(){
+    window.location.href = 'Illumination/illumination.html';
+});
+document.getElementById('_button2e').addEventListener('click', function(){
+    window.location.href = 'SearchBar/searchBar.html';
+});
+function redirectToIndex() {
+    window.location.href = "../index.html";
+}
